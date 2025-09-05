972c331b7ed77878e159f4140202519b9139db5d
Sh1v4nk <shivankpandey113@gmail.com>
2025-09-05 18:10:34 +0530

    configure webpack to ignore pdf-parse test files during server-side bundling

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    webpack: (config, { isServer }) => {
+        if (isServer) {
+            // Ignore pdf-parse test files during bundling
+            config.module.rules.push({
+                test: /pdf-parse/,
+                use: "ignore-loader",
+            });
+        }
+        return config;
+    },
+};
 
 export default nextConfig;
